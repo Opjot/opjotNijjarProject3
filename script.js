4df41da33ce5d51016f2b5c27bfa879723f2e6c1
Opjot Singh Nijjar <opjot-singh-nijjar@Opjots-MacBook-Pro.local>
2020-02-12 19:36:17 -0500

    fully functional application

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-    
     function number(min, max){
         min = Math.ceil(min);
         max = Math.floor(max);
@@ -25,19 +24,13 @@ $(document).ready(function(){
     $(".num3").append(numberArray[2])
     $(".num4").append(numberArray[3])
 
-    // console.log(numberArray);
     // grabbing the random numbers generated and putting them into an array.
-
     const generatedNumbersCalled = [];
 
     const numbersGenerated = [];
     for(let i = 0; i <= 10; i++){
         numbersGenerated.push(i);
-        // if (i === 12){
-        //     alert("All numbers have been called");
-        // }
     }
-
 
     $(".next").on("click", function(){
         let numberDrawn = number(0, numbersGenerated.length - 1)
@@ -47,18 +40,29 @@ $(document).ready(function(){
         generatedNumbersCalled.push(removedItem)
         console.log(generatedNumbersCalled)
         // numbersGenerated.push(numberDrawn)
-        if (generatedNumbersCalled.length >= 11 ){
+        if (generatedNumbersCalled.length >= 12 ){
             alert("All numbers have been called!")
         }
     })
 
+    $(".bingo").on("click", function (){ 
+       function checkArrays(array1, array2){
+        array2 = array2.flat();
+        return array1.every(function(item){
+                console.log(item)
+                return array2.includes(item);
+            })
+        }
 
-    // $(".bingo").on(click, function compare(array1, array2){
-    //     const arrayCheck = []
-
-    // })
-
-
+            if(checkArrays(numberArray, generatedNumbersCalled) === true){
+                alert("You have won the game!")
+            }
+            else (
+                alert("All of your numebrs dont match!")
+            )
+            console.log(checkArrays(numberArray, generatedNumbersCalled));
+            }) 
+    
     // changed the css styling on click.
     $(".gridItem").on("click", function(){
         $(this).toggleClass("selected")
